c0877e4a5cef18cd1a9a76b06b4aa4600005410a
Fernando Jorge <fernandojorge.cavalcantegomes@gmail.com>
2023-04-19 21:42:24 -0300

    add developer badge

---
+++ src/components/badges/DeveloperBadge.jsx
@@ -0,0 +1,10 @@
+export default function DeveloperBadge() {
+  return (
+    <p className="DeveloperBadge">
+      <img
+        src="https://raw.githubusercontent.com/nandowastaken/icons-storage/c98e5c6d1c3c1dadb0081ae1c24336ae84729c80/monitore-me/DeveloperBadge.svg"
+        alt="Insígnia de Desenvolvedor."
+      />
+    </p>
+  );
+}
